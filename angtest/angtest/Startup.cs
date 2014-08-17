a5069c6f3f619b27ea72256cb57576ff6e45ab30
ssnkumar3 <ssn.narayanan@hotmail.com>
2014-08-17 13:33:49 +0530

    from scratch

---
+++ angtest/angtest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(angtest.Startup))]
+namespace angtest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
